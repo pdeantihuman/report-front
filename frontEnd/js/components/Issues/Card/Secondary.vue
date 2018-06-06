dadd103456a4c7ef9461e081bf628023aac6cb57
pdeantihuman <pdeantihuman@gmail.com>
2018-06-06 23:54:38 +0800

    complete secondary

---
+++ frontEnd/js/components/Issues/Card/Secondary.vue
@@ -0,0 +1,32 @@
+<template>
+    <b-btn
+            class="mt-3"
+            v-bind:class="buttonClass"
+            >
+        下一个
+    </b-btn>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+            }
+        },
+        computed: {
+            buttonClass() {
+                return this.is_grey?"outline-secondary":"outline-danger"
+            },
+        },
+        props: {
+            is_grey: Boolean,
+            issue_id: String,
+        },
+        methods: {
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
