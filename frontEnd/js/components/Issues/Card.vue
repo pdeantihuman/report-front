437c97433e980a3c6355830a0b25a134d0a9c7c4
pdeantihuman <pdeantihuman@gmail.com>
2018-06-06 22:43:55 +0800

    complete primary

---
+++ frontEnd/js/components/Issues/Card.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="card-body">
+        <div class="d-block text-center">
+            <div class="d-block text-center">
+                <h4> {{ location }} </h4>
+                <h6> {{ datetime }} </h6>
+                <h5> {{ description }} </h5>
+            </div>
+            <primary issue_id={{ issue_id }}></primary>
+        </div>
+    </div>
+</template>
+
+<script>
+    import from './Card/Primary'
+    export default {
+        data() {
+            return {}
+        },
+        components: {
+            'primary' : Primary
+        },
+        props: {
+            issue_id: String,
+            location: String,
+            description: String,
+            datetime: String,
+            completed: String,
+            next_id: String
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
