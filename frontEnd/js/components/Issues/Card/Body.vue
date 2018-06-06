41f05436d349f07ed8f08f0c8c110980bcc7aa68
pdeantihuman <pdeantihuman@gmail.com>
2018-06-06 23:54:47 +0800

    import secondary

--- frontEnd/js/components/Issues/Card/Body.vue
+++ frontEnd/js/components/Issues/Card/Body.vue
@@ -6,13 +6,15 @@
                 <h6> {{ datetime }} </h6>
                 <h5> {{ description }} </h5>
             </div>
-            <primary issue_id={{ issue_id }}></primary>
+            <primary :issue_id="issue_id"></primary>
+            <secondary :is_grey="completed == 1" :issue_id="next_id"></secondary>
         </div>
     </div>
 </template>
 
 <script>
     import from './Card/Primary'
+    import from './Card/Secondary'
     export default {
         data() {
             return {
@@ -20,15 +22,17 @@
             }
         },
         components: {
-            'primary' : Primary
+            'primary' : Primary,
+            'secondary' : Secondary
         },
         props: {
             issue_id: String,
             location: String,
             description: String,
             datetime: String,
-            completed: String,
-            next_id: String
+            completed: String, // 子组件
+            next_id: String, // 子组件
+            completed: String // 子组件
         },
         computed: {
 
