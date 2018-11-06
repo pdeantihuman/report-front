d187838781257e7dec0ef63f42f721ccc108b1b1
pdeantihuman <pdeantihuman@gmail.com>
2018-11-06 23:59:42 +0800

    install and enable browserSync 安装并启用 browserSync

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,9 +11,19 @@ let mix = require('laravel-mix');
  |
  */
 
-// mix.js('frontEnd/js/index/index.js', 'public/js')
-//    .sass('frontEnd/css/app.scss', 'public/css')
-// ;
+mix.js('frontEnd/js/index/index.js', 'public/js');
+mix.sass('frontEnd/css/app.scss', 'public/css');
 mix.js('frontEnd/js/show/show.js', 'public/js')
-   // .sass('frontEnd/css/app.scss', 'public/css')
-;
+//     .sass('frontEnd/css/app.scss', 'public/css');
+mix.js('frontEnd/js/app/app.js', 'public/js');
+mix.browserSync({
+    proxy: 'report',
+    serveStatic: [{
+        route: '/js',
+        dir: 'public/js'
+    }],
+    port: '8888',
+    files: [
+        'public'
+    ]
+})
